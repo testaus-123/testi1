2236de1e022738354aac5c12fb65d86e686e0cdb
miikka <miikka@192.168.1.3>
2020-10-19 22:52:18 +0300

    commit 2

--- NetBeansProjects/LaskuriProjekti/src/Laskuri.java
+++ NetBeansProjects/LaskuriProjekti/src/Laskuri.java
@@ -22,9 +22,15 @@ public class Laskuri{
 		this.tapahtumia = 0;
 	}
 
+
   	public void lisaa(){
     		this.tapahtumia = this.tapahtumia + 1; 
   	}
+        
+        public void lisaa(int lisays) {
+            this.tapahtumia=this.tapahtumia+-lisays;
+        
+        }
   	public int lukema(){
     		return this.tapahtumia;
   	}
@@ -32,7 +38,7 @@ public class Laskuri{
     		this.tapahtumia = 0;
   	}
 	public String toString(){
-		return "Laskurin lukema on " + this.tapahtumia;
+		return "Lukema " + this.tapahtumia;
 	}
 
 }
